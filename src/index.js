f5e9445ddade01cc1dfe712d22a9072983614a78
serenenity <mopaul@student.lautech.edu.ng>
2023-12-03 00:16:56 +0100

    setup working dir

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.js';
+
+
+// const { web5, did: aliceDid } = await Web5.connect();
+// const { record } = await web5.dwn.records.create({
+//   data: 'Hello, Web5!',
+//   message: {
+//     dataFormat: 'text/plain',
+//   },
+// });
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// reportWebVitals();
+// import reportWebVitals from '../reportWebVitals';
+// import { Web5 } from '@web5/api';
+// import { webcrypto } from 'node:crypto';
+
+
+// if (!globalThis.crypto) globalThis.crypto = webcrypto;
